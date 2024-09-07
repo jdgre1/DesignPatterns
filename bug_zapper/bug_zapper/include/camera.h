5e8c90cfebdea793a87973c5619ba884e0704f78
James Gregory <james.gregory@naska.io>
2024-09-07 14:11:56 +0200

    "Started a control system example"

---
+++ bug_zapper/bug_zapper/include/camera.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+namespace pi_robot {
+	
+class Camera {
+  
+	public:
+	    size_t id;
+	    // CameraInfo cameraInfo;
+	    // CalibrationParameters calibParams
+	    // Transform transform
+	    // Resolution res
+	    //
+};
+	
+}
---
+++ bug_zapper/bug_zapper/src/main.cpp
@@ -0,0 +1,20 @@
+#include <bug_zapper.h>
+
+int main(int argc, char* argv[])
+{
+    // auto pl = std::make_shared<patterns::Player>();
+    // patterns::EntityManager& em = patterns::EntityManager::GetInstance();
+    // patterns::Viewer& viewer = patterns::Viewer::GetInstance(em, patterns::FIELD_HEIGHT_MAX, patterns::FIELD_WIDTH_MAX);
+    // patterns::Controller& cntrlr = patterns::Controller::GetInstance(pl);
+    // patterns::MovementSystem& ms = patterns::MovementSystem::GetInstance(em);
+    // patterns::Menu& menu = patterns::Menu::GetInstance();
+    // patterns::Game& game = patterns::Game::GetInstance(em, menu,  cntrlr, viewer, ms, pl);
+
+    // game.start();
+    // while (game.isRunning()) {
+    //     game.readInput();
+    //     game.update();
+    //     game.render();
+    // }
+    return 0;
+}
