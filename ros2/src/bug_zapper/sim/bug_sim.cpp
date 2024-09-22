0c5bdad886cc5a6173fa1d800af3b6a040fa7520
James Gregory <james.gregory@naska.io>
2024-09-22 14:14:36 +0200

    "Further updates to bug-sim"

--- ros2/src/bug_zapper/sim/bug_sim.cpp
+++ ros2/src/bug_zapper/sim/bug_sim.cpp
@@ -1,104 +1,149 @@
-#include <random> 
-#include <opencv2/core/core.hpp>
-
 #include <bug_factory.h>
 #include <bug_sim.h>
+
+#include <random>
 
 using namespace std::chrono_literals;
 namespace patterns
 {
 
-    int GenerateRandomNumberBetween(int min, int max)
+int GenerateRandomNumberBetween(int min, int max)
+{
+    // Create a random device and seed the generator
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+
+    // Define the range 1 to 3 inclusive
+    std::uniform_int_distribution<> dis(min, max);
+
+    // Generate and return the random numberII
+    return dis(gen);
+}
+
+BugSim::BugSim(double bugSpeedMin, double bugSpeedMax, uint8_t bugStrength)
+    : rclcpp_lifecycle::LifecycleNode("bug_sim_lifecycle_node"), m_bugSpeedMin(bugSpeedMin), m_bugSpeedMax(bugSpeedMax),
+      m_bugStrength(bugStrength)
+{
+    m_startTime = this->get_clock()->now();
+    m_timer = this->create_wall_timer(100ms, std::bind(&BugSim::simTimerCallback, this));
+}
+
+void BugSim::DrawBug(std::shared_ptr<Bug> bug, cv::Mat &frame, const int &timeMsPassed)
+{
+    Components::XYComponent &xyPos = bug->getPos();
+    uint8_t speed = bug->getSpeed();
+    xyPos.posY += speed * timeMsPassed / 1000.0;
+
+    switch (bug->getType()) {
+    case BugType::Alien:
     {
-        // Create a random device and seed the generator
-        static std::random_device rd;
-        static std::mt19937 gen(rd());
-
-        // Define the range 1 to 3 inclusive
-        std::uniform_int_distribution<> dis(min, max);
-
-        // Generate and return the random number
-        return dis(gen);
-
+        cv::Point center(xyPos.posX, xyPos.posY);
+        cv::uint8_t radius = bug->getSize();
+        cv::Scalar line_Color(255, 0, 0);
+        int thickness = 2;
+        cv::circle(frame, center, radius, line_Color, thickness);
     }
 
-    void BugSim::AddRandomBug(BugType& bugtype)
+    case BugType::Zipper:
     {
-        u_int8_t speed = 0;
-        u_int8_t strength = 0;
-        u_int8_t size = 0;
-        
-        switch(bugtype)
-            {
-                
-            case BugType::Alien:
-            {
-                speed = static_cast<u_int8_t>(GenerateRandomNumberBetween(3,5));
-                strength = static_cast<u_int8_t>(GenerateRandomNumberBetween(3,5));
-                size = static_cast<u_int8_t>(GenerateRandomNumberBetween(5,7));
-            }
-
-            case BugType::Zipper:
-            {
-                speed = static_cast<u_int8_t>(GenerateRandomNumberBetween(5,7));
-                strength = static_cast<u_int8_t>(GenerateRandomNumberBetween(1,3));
-                size = static_cast<u_int8_t>(GenerateRandomNumberBetween(2,3));
-            }
-
-            case BugType::BigBertha:
-            {
-                speed = static_cast<u_int8_t>(GenerateRandomNumberBetween(1,3));
-                strength = static_cast<u_int8_t>(GenerateRandomNumberBetween(5,8));
-                size = static_cast<u_int8_t>(GenerateRandomNumberBetween(5,8));
-            }
-
-            default:
-            {
-                std::cout << "\nBug type does not exist!";
-                return;
-            }
-            }
-
-        m_bugs.push_back(m_bugfactory.CreateBug(bugtype, size, speed, strength));
-
+        cv::Point center(xyPos.posX, xyPos.posY);
+        uint8_t radius = bug->getSize();
+        cv::Scalar line_Color(0, 0, 255);
+        int thickness = 2;
+        cv::circle(frame, center, radius, line_Color, thickness);
     }
 
-    BugSim::BugSim(double bugSpeedMin, double bugSpeedMax, uint8_t bugStrength)
-    : rclcpp_lifecycle::LifecycleNode("bug_sim_lifecycle_node")
-        , m_bugSpeedMin(bugSpeedMin)
-        , m_bugSpeedMax(bugSpeedMax)
-        , m_bugStrength(bugStrength)
+    case BugType::BigBertha:
     {
-        m_startTime = this->get_clock()->now();
-        m_timer =  this->create_wall_timer(100ms, std::bind(&BugSim::simTimerCallback, this));
+        cv::Point center(xyPos.posX, xyPos.posY);
+        uint8_t radius = bug->getSize();
+        cv::Scalar line_Color(0, 255, 0);
+        int thickness = 2;
+        cv::circle(frame, center, radius, line_Color, thickness);
     }
 
-
-    void BugSim::simTimerCallback()
+    default:
     {
-        cv::Mat cameraFrame(cv::Size(1000,800), CV_8UC3, cv::Scalar(255,255,255));
-        int diffMs = ((this->get_clock()->now() - m_startTime).to_chrono<std::chrono::milliseconds>()).count();
-       
-        size_t numBugs = m_bugs.size();
-        if (numBugs < 3 && diffMs % m_bugSpawnIntervalMs == 0) {
-            BugType randomBugType = static_cast<BugType>(GenerateRandomNumberBetween(1, 3));
-            AddRandomBug(randomBugType);
+        std::cout << "\nBug type does not exist!";
+        return;
+    }
+    }
+}
+
+void BugSim::AddRandomBug(BugType &bugtype)
+{
+    u_int8_t speed = 0;
+    u_int8_t strength = 0;
+    u_int8_t size = 0;
+    int32_t xPos = static_cast<int32_t>(
+        GenerateRandomNumberBetween(BUG_OFFSET_FROM_WIDTH_PIXELS, FIELD_WIDTH_PIXELS - BUG_OFFSET_FROM_WIDTH_PIXELS));
+
+    switch (bugtype) {
+    case BugType::Alien:
+    {
+        speed = static_cast<u_int8_t>(GenerateRandomNumberBetween(3, 5));
+        strength = static_cast<u_int8_t>(GenerateRandomNumberBetween(3, 5));
+        size = static_cast<u_int8_t>(GenerateRandomNumberBetween(5, 7));
+    }
+
+    case BugType::Zipper:
+    {
+        speed = static_cast<u_int8_t>(GenerateRandomNumberBetween(5, 7));
+        strength = static_cast<u_int8_t>(GenerateRandomNumberBetween(1, 3));
+        size = static_cast<u_int8_t>(GenerateRandomNumberBetween(2, 3));
+    }
+
+    case BugType::BigBertha:
+    {
+        speed = static_cast<u_int8_t>(GenerateRandomNumberBetween(1, 3));
+        strength = static_cast<u_int8_t>(GenerateRandomNumberBetween(5, 8));
+        size = static_cast<u_int8_t>(GenerateRandomNumberBetween(5, 8));
+    }
+
+    default:
+    {
+        std::cout << "\nBug type does not exist!";
+        return;
+    }
+    }
+
+    m_bugs.push_back(m_bugfactory.CreateBug(bugtype, size, speed, strength));
+}
+
+void BugSim::simTimerCallback()
+{
+    cv::Mat cameraFrame(cv::Size(FIELD_WIDTH_PIXELS, FIELD_LENGTH_PIXELS), CV_8UC3, cv::Scalar(255, 255, 255));
+    const int diffMs = ((this->get_clock()->now() - m_startTime).to_chrono<std::chrono::milliseconds>()).count();
+
+    size_t numBugs = m_bugs.size();
+    if (numBugs < 3 && diffMs % m_bugSpawnIntervalMs == 0) {
+        BugType randomBugType = static_cast<BugType>(GenerateRandomNumberBetween(1, 3));
+        AddRandomBug(randomBugType);
+    }
+    int bugToDelete = -1;
+    int counter = 0;
+    for (std::shared_ptr<Bug> bug : m_bugs) {
+        // ~~~ ToDo ~~~
+        // uint8_t bugSize = bug->getSize();
+        // uint8_t bugSpeed = bug->getSpeed();
+        // uint8_t bugStrength = bug->getStrength();
+        Components::XYComponent &posXYBug = bug->getPos();
+        if (posXYBug.posY - bug->getSize() > cameraFrame.rows) {
+            bugToDelete = counter;
+            continue;
         }
-
-        for (std::shared_ptr<Bug> bug: m_bugs) 
-        {
-            // ~~~ ToDo ~~~
-            uint8_t bugSize = bug->getSize();
-            uint8_t bugSpeed = bug->getSpeed();
-            uint8_t bugStrength = bug->getStrength();
-            
-
-            // bug.updatePos();
-        }
+        DrawBug(bug, cameraFrame, diffMs);
+        counter++;
     }
-
-
-
-            // std::unique_ptr<Bug> newBug = CreateBug(randomBugType, bugSize, speed, strength) 
+    if (bugToDelete > -1) {
+        m_bugs.erase(m_bugs.begin() + bugToDelete);
     }
-    
+    cv::namedWindow("Bug-Frame");
+
+    cv::imshow("Bug-Frame", cameraFrame);
+    cv::waitKey(diffMs);
+}
+
+// std::unique_ptr<Bug> newBug = CreateBug(randomBugType, bugSize, speed,
+// strength)
+} // namespace patterns
--- ros2/src/bug_zapper/sim/bug_sim.h
+++ ros2/src/bug_zapper/sim/bug_sim.h
@@ -3,6 +3,9 @@
 #define BUGSIM_H
 
 #include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <vector>
 
 #include <rclcpp/rclcpp.hpp>
@@ -13,16 +16,15 @@
 namespace patterns
 {
 
-
 class BugSim : public rclcpp_lifecycle::LifecycleNode
 {
 public:
     BugSim(double bugSpeedMin, double bugSpeedMax, uint8_t m_bugStrength);
-    
 
 private:
     void simTimerCallback();
-    void AddRandomBug(BugType& bugtype);
+    void AddRandomBug(BugType &bugtype);
+    void DrawBug(std::shared_ptr<Bug> bug, cv::Mat &frame, const int &timePassedMs);
 
     double m_bugSpeedMin;
     double m_bugSpeedMax;
@@ -33,7 +35,10 @@ private:
     rclcpp::TimerBase::SharedPtr m_timer;
     std::vector<std::shared_ptr<Bug>> m_bugs;
     const int m_bugSpawnIntervalMs = 4000;
+    const int FIELD_WIDTH_PIXELS = 1000;
+    const int FIELD_LENGTH_PIXELS = 1800;
+    const int BUG_OFFSET_FROM_WIDTH_PIXELS = 50;
 };
 
-}  // namespace patterns
+} // namespace patterns
 #endif
